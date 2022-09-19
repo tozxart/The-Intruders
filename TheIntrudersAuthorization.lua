b57aa606a2f1942290dd9626be081669c11406d3
tozxart <69420764+tozxart@users.noreply.github.com>
2022-09-19 04:07:00 +0100

    Update TheIntrudersAuthorization.lua

--- TheIntrudersAuthorization.lua
+++ TheIntrudersAuthorization.lua
@@ -65,7 +65,7 @@ _G.Settings = {
     kickrejoin = true
 }
 
-if game.PlaceId == 6938803436 or game.PlaceId == 7338881230 or game.PlaceId == 6990129309 or game.PlaceId == 7274690025 or game.PlaceId == 6990133340 then
+if game.PlaceId == 6938803436 or game.PlaceId == 7338881230 or game.PlaceId == 6990129309 or game.PlaceId == 7274690025 or game.PlaceId == 6990131029 or game.PlaceId == 6990133340 then
     pcall(function()
            loadstring(game:HttpGet("https://raw.githubusercontent.com/tozxart/SunHub/main/TheIntruders.lua"))()
     end)
