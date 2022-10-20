29bc703d9e4154859b35346833ffdd81c209ddee
tozxart <69420764+tozxart@users.noreply.github.com>
2022-10-20 13:04:55 +0100

    Update TheIntrudersAuthorizationn.lua

--- TheIntrudersAuthorizationn.lua
+++ TheIntrudersAuthorizationn.lua
@@ -66,6 +66,6 @@ _G.Settings = {
 }
 if game.PlaceId == 6938803436 or game.PlaceId == 7338881230 or game.PlaceId == 6990129309 or game.PlaceId == 7274690025 or game.PlaceId == 6990131029 or game.PlaceId == 6990133340 then
     pcall(function()
-           loadstring(game:HttpGet("https://raw.githubusercontent.com/tozxart/SunHub/main/TheIntruders.lua"))()
+           loadstring(game:HttpGet("https://raw.githubusercontent.com/tozxart/The-Intruders/main/TheIntruders.lua"))()
     end)
 end
