d82238d252443bacab0e7ecf1bd89cee69825ecc
tozxart <fallag.rayen@gmail.com>
2023-04-10 01:19:29 +0000

    Update SaveConfig.lua

--- SaveConfig.lua
+++ SaveConfig.lua
@@ -2,36 +2,55 @@ local Data = {}
 local DataFunctions = {}
 local Http = game:GetService("HttpService")
 
-function Data.new(name, data)
-	if not isfolder(name) then
-		makefolder(name)
-	end
+function Data.new(folderName, data)
+    if not isfolder(folderName) then
+        makefolder(folderName)
+    end
 
-    local savedData = isfile(name.."/settings.json") and Http:JSONDecode(readfile(name.."/Settings.json"))
-    
+    local savedData, errorMsg = pcall(function()
+        return Http:JSONDecode(readfile(folderName .. "/Settings.json"))
+    end)
+
     if savedData then
-        for i,v in pairs(data) do
+        for i, v in pairs(data) do
             if not savedData[i] then
                 savedData[i] = v
             end
         end
+    else
+        savedData = data
     end
 
-	return setmetatable({
-		Data = savedData or data,
-		FolderName = name
-	}, {
-		__index = DataFunctions
-	})
+    return setmetatable({
+        Data = savedData,
+        FolderName = folderName
+    }, {
+        __index = DataFunctions
+    })
 end
 
 function DataFunctions:Set(name, value)
-	self.Data[name] = value
-	writefile(self.FolderName.."/Settings.json", Http:JSONEncode(self.Data))
+    local success, errorMsg = pcall(function()
+        self.Data[name] = value
+        writefile(self.FolderName .. "/Settings.json", Http:JSONEncode(self.Data))
+    end)
+
+    if not success then
+        warn("Error while setting value:", errorMsg)
+    end
 end
 
 function DataFunctions:Get(name)
-	return self.Data[name]
+    local success, result = pcall(function()
+        return self.Data[name]
+    end)
+
+    if success then
+        return result
+    else
+        warn("Error while getting value:", result)
+        return nil
+    end
 end
 
 return Data
