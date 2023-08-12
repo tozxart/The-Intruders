58c239cd907882773d6ba88abb494eb21a5c110c
tozxart <69420764+tozxart@users.noreply.github.com>
2023-08-12 16:29:37 -0400

    up

---
+++ ChangeLog/ADSChangeLog.lua
@@ -0,0 +1,7 @@
+return {
+    News = { '<b><font color="rgb(255,0,0)">V 0.2</font></b>', '> Add Auto Dode', '> Auto Skills' },
+    Title = 'The Intruders',
+    MainText = 'Make Sure to join our <b>Discord Server</b>.<br/><font color="rgb(0,155,225)">https://discord.gg/vfkD5VCRKU</font>',
+    DelayToDestroy = 15,
+    From = 'ToZxart', 
+}
