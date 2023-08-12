e4016fec516cf77ed248e471fb7071e4f3b17efc
tozxart <69420764+tozxart@users.noreply.github.com>
2023-08-12 23:04:10 +0100

    Update ADSChangeLog.lua

--- ChangeLog/ADSChangeLog.lua
+++ ChangeLog/ADSChangeLog.lua
@@ -2,22 +2,22 @@ return {
     Version = "1.0", -- Change this to the version of your script
     News = { 
         '<b><font color="rgb(255,0,0)">V 0.2</font></b>',
-        '> New AI Suggestions: Get helpful messages on Discord to improve your gameplay and become better at the game.'
-        '> Free Auto Dodge Update: Apologies, but the auto dodge will stop working after 4 hours. You ll need to get a new key to make it work again. If the auto dodge expires, the TP Behind feature will activate automatically.'
-        '> Updated Select Raid and Dimension: Now you can choose them whenever you want.'
-        '> Ive made many improvements and fixed bugs and everything above.'
+        '> New AI Suggestions: Get helpful messages on Discord to improve your gameplay and become better at the game.',
+        '> Free Auto Dodge Update: Apologies, but the auto dodge will stop working after 4 hours. You ll need to get a new key to make it work again. If the auto dodge expires, the TP Behind feature will activate automatically.',
+        '> Updated Select Raid and Dimension: Now you can choose them whenever you want.',
+        '> Ive made many improvements and fixed bugs and everything above.',
         '<b><font color="rgb(255,0,0)">V 0.1</font></b>',
         '> Updated the wishlist System',
-        '> Fixed Auto Dodge' 
-        '> New Auto Delete mass cards' 
-        '> New Auto Upgrade Cards'
-        '> New auto Equip best cards with some advance stats'
-        '> Webhook for Upgrade, Delete, Equip cards'
-        '> Removed Prem Tab'
-        '> New Reset Webhook id'
-        '> New reset setting in the credit tab it will show when the script load'
-        '> New hide UI / Minimize ui'
-        '> New Auto leave'
+        '> Fixed Auto Dodge' ,
+        '> New Auto Delete mass cards' ,
+        '> New Auto Upgrade Cards',
+        '> New auto Equip best cards with some advance stats',
+        '> Webhook for Upgrade, Delete, Equip cards',
+        '> Removed Prem Tab',
+        '> New Reset Webhook id',
+        '> New reset setting in the credit tab it will show when the script load',
+        '> New hide UI / Minimize ui',
+        '> New Auto leave',
     },
     Title = 'The Intruders',
     MainText = 'Make Sure to join our <b>Discord Server</b>.<br/><font color="rgb(0,155,225)">https://discord.gg/vfkD5VCRKU</font>',
