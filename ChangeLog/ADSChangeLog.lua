370777f19993f8e49ae6da23cd54f563d173e685
tozxart <69420764+tozxart@users.noreply.github.com>
2023-08-12 16:37:15 -0400

    up

--- ChangeLog/ADSChangeLog.lua
+++ ChangeLog/ADSChangeLog.lua
@@ -1,4 +1,5 @@
 return {
+    Version = "1.0", -- Change this to the version of your script
     News = { '<b><font color="rgb(255,0,0)">V 0.2</font></b>', '> Add Auto Dode', '> Auto Skills' },
     Title = 'The Intruders',
     MainText = 'Make Sure to join our <b>Discord Server</b>.<br/><font color="rgb(0,155,225)">https://discord.gg/vfkD5VCRKU</font>',
