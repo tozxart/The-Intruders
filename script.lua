7804df59c8aa811851214ba951f4da59c15867e5
tozxart <69420764+tozxart@users.noreply.github.com>
2023-09-01 14:22:11 +0100

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,2 +1,46 @@
-repeat wait() until game:IsLoaded()
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/3afb7bf7c1aac757c597d415975d8955.lua"))()
+repeat
+    wait()
+until game:IsLoaded()
+
+local scriptBaseUrl = "https://api.luarmor.net/files/v3/loaders/"
+
+local games = {
+    ADS = {
+        gameID = 2655311011,
+        scriptUrl = scriptBaseUrl .. "3afb7bf7c1aac757c597d415975d8955.lua",
+    },
+    aNIMElEGACY = {
+        gameID = 4844706238,
+        scriptUrl = scriptBaseUrl .. "297f5364acdb60ae4484561bb1edfdb4.lua",
+    },
+    PunshWallSimulator = {
+        gameID = 4498728505,
+        scriptUrl = scriptBaseUrl .. "",
+    },
+    ACS = {
+        gameID = 4280746206,
+        scriptUrl = scriptBaseUrl .. "f45402e68e94c2a10cb6a357b30147b0.lua",
+    },
+    WFS = {
+        gameID = 3262314006,
+        scriptUrl = scriptBaseUrl .. "ca4ad6aaf0e289e9c28f4953018f188d.lua",
+    },
+}
+
+
+
+for gameName, gameData in pairs(games) do
+    if gameData.gameID == game.GameId then
+        local success, result = pcall(function()
+            return loadstring(game:HttpGet(gameData.scriptUrl))
+        end)
+        
+        if success then
+            result()
+        else
+            warn("Failed to load and execute script for " .. gameName)
+        end
+        
+        break
+    end
+end
