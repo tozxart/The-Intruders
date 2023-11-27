ce6c57321cf3518b1b6cd718d592ae8d6d72b4f4
tozxart <69420764+tozxart@users.noreply.github.com>
2023-11-27 14:09:03 +0100

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,55 +1,47 @@
-
-game.StarterGui:SetCore("SendNotification", {
-Title = "Error"; -- the title (ofc)
-Text = "Please join discord and use the new link to get the key"; 
-Icon = "rbxassetid://57254792"; 
-Duration = 30;
-})
-
-
 repeat
     wait()
 until game:IsLoaded()
+ game.Players.LocalPlayer:Kick(
+            "To access the script, please join our Discord server and get the paid version: discord.com/invite/vfkD5VCRKU.")
+-- local scriptBaseUrl = "https://api.luarmor.net/files/v3/loaders/"
 
-local scriptBaseUrl = "https://api.luarmor.net/files/v3/loaders/"
-
-local games = {
-    ADS = {
-        gameID = 2655311011,
-        scriptUrl = scriptBaseUrl .. "3afb7bf7c1aac757c597d415975d8955.lua",
-    },
-    aNIMElEGACY = {
-        gameID = 4844706238,
-        scriptUrl = scriptBaseUrl .. "297f5364acdb60ae4484561bb1edfdb4.lua",
-    },
-    PunshWallSimulator = {
-        gameID = 4498728505,
-        scriptUrl = scriptBaseUrl .. "",
-    },
-    ACS = {
-        gameID = 4280746206,
-        scriptUrl = scriptBaseUrl .. "f45402e68e94c2a10cb6a357b30147b0.lua",
-    },
-    WFS = {
-        gameID = 3262314006,
-        scriptUrl = scriptBaseUrl .. "ca4ad6aaf0e289e9c28f4953018f188d.lua",
-    },
-}
+-- local games = {
+--     ADS = {
+--         gameID = 2655311011,
+--         scriptUrl = scriptBaseUrl .. "3afb7bf7c1aac757c597d415975d8955.lua",
+--     },
+--     aNIMElEGACY = {
+--         gameID = 4844706238,
+--         scriptUrl = scriptBaseUrl .. "297f5364acdb60ae4484561bb1edfdb4.lua",
+--     },
+--     PunshWallSimulator = {
+--         gameID = 4498728505,
+--         scriptUrl = scriptBaseUrl .. "",
+--     },
+--     ACS = {
+--         gameID = 4280746206,
+--         scriptUrl = scriptBaseUrl .. "f45402e68e94c2a10cb6a357b30147b0.lua",
+--     },
+--     WFS = {
+--         gameID = 3262314006,
+--         scriptUrl = scriptBaseUrl .. "ca4ad6aaf0e289e9c28f4953018f188d.lua",
+--     },
+-- }
 
 
 
-for gameName, gameData in pairs(games) do
-    if gameData.gameID == game.GameId then
-        local success, result = pcall(function()
-            return loadstring(game:HttpGet(gameData.scriptUrl))
-        end)
+-- for gameName, gameData in pairs(games) do
+--     if gameData.gameID == game.GameId then
+--         local success, result = pcall(function()
+--             return loadstring(game:HttpGet(gameData.scriptUrl))
+--         end)
         
-        if success then
-            result()
-        else
-            warn("Failed to load and execute script for " .. gameName)
-        end
+--         if success then
+--             result()
+--         else
+--             warn("Failed to load and execute script for " .. gameName)
+--         end
         
-        break
-    end
-end
+--         break
+--     end
+-- end
