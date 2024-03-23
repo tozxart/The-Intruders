de69703a84aa701814db653a02060bd5ad59af5b
tozxart <69420764+tozxart@users.noreply.github.com>
2024-03-23 16:24:38 +0100

    Update ADSChangeLog.lua

--- ChangeLog/ADSChangeLog.lua
+++ ChangeLog/ADSChangeLog.lua
@@ -1,6 +1,13 @@
 return {
-    Version = "1.05", -- Change this to the version of your script
+    Version = "1.08", -- Change this to the version of your script
     News = { 
+         '<b><font color="rgb(255,0,0)">V 1.08</font></b>',
+        '> Added The New Version of the script.',
+        '> Added Auto Fruit 1 and 2 with 2 methods.',
+        '> Added Auto Raid Challange .',
+        '> Added New Map to the auto level (I will make it automatic later) .',
+        '> I will fix the raid challenge webhook later.',
+        '> script now back to paid plan and free version too using linkvertise.',
          '<b><font color="rgb(255,0,0)">V 1.07</font></b>',
         '> Added Auto Madoki.',
         '> Webhooks now works better and it will give you more info and it will mention you when you get rare stuff.',
