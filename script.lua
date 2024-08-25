c1b50aec4b288a0b26d178e84e14f8e5f6059979
ToZx <69420764+tozxart@users.noreply.github.com>
2024-08-25 07:26:37 +0100

    up

--- script.lua
+++ script.lua
@@ -47,16 +47,20 @@ for gameName, gameData in pairs(games) do
 end
 local TurtleNotifications = loadstring(game:HttpGet("https://raw.githubusercontent.com/Turtle-Brand/Turtle-Notifications/main/source.lua"))()
 
-local NotificationLibrary = TurtleNotifications.new(false, 2)
+-- Create a coroutine to run the notification script
+coroutine.wrap(function()
+    local NotificationLibrary = TurtleNotifications.new(false, 2)
 
-NotificationLibrary:QueueNotification(20, "Update Link!", "Get your free key here: https://go.click.ly/Jxeyd. For more info, join our Discord!", "Cancel-Copy", {
-    Cancel = function()
-        print("User chose to ignore the notification.")
-    end,
-    Copy = function()
-        print("User clicked to copy the free key link.")
-        setclipboard("https://go.click.ly/Jxeyd")
-        print("Free key link copied to clipboard.")
-    end
-})
-NotificationLibrary:SetNotificationDelay(20)
+    NotificationLibrary:QueueNotification(20, "Update Link!", "Get your free key here: https://go.click.ly/Jxeyd. For more info, join our Discord!", "Cancel-Copy", {
+        Cancel = function()
+            print("User chose to ignore the notification.")
+        end,
+        Copy = function()
+            print("User clicked to copy the free key link.")
+            setclipboard("https://go.click.ly/Jxeyd")
+            print("Free key link copied to clipboard.")
+        end
+    })
+
+    NotificationLibrary:SetNotificationDelay(20)
+end)()
