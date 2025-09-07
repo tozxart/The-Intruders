3d7ebad3e35e6a78daf134d7a697f4fdf1a2bd91
tozxart <fallag.rayen@gmail.com>
2025-09-07 06:54:01 +0100

    Refactor script.lua to load game scripts dynamically and remove ScriptNOexe. Update ADSChangeLog.lua to version 1.12 with new features and improvements.

--- ChangeLog/ADSChangeLog.lua
+++ ChangeLog/ADSChangeLog.lua
@@ -1,26 +1,29 @@
-{
-    "Version": "1.11",
-    "News": [
-     "V 1.11 - 3/29/2024",
-      "> Fixed Auto Swap Team.",
-      "V 1.10 - 3/29/2024",
-      "> Fixed Auto Dodge stop working in the last map(hope so :).",
-      "> Fixed user info webhook show wrong informations.",
-      "> Fixed UI issue that makes some executors does not work.",
-      "V 1.09 - 3/28/2024",
-      "> Added Hide Button For Mobile Users.",
-      "> Improvet the GUI.",
-      "> Improvet Auto Fruit.",
-      "V 1.08 - 3/24/2024",
-      "> Added The New Version of the script.",
-      "> Added Auto Fruit 1 and 2 with 2 methods.",
-      "> Added Auto Raid Challenge.",
-      "> Added New Map to the auto level (I will make it automatic later).",
-      "> I will fix the raid challenge webhook later.",
-      "> script now back to a paid plan and free version to using linkvertise."
-    ],
-    "Title": "The Intruders",
-    "MainText": "Make Sure to join our <b>Discord Server</b>.<br/><font color=\"rgb(0,155,225)\">https://discord.gg/vfkD5VCRKU</font>",
-    "DelayToDestroy": 15,
-    "From": "ToZxart"
-  }
+return {
+  Version = "1.12",
+  News = {
+    "V 1.12 - 9/07/2025",
+    "> All features are now FREE for everyone!",
+    "V 1.11 - 3/29/2024",
+    "> Fixed Auto Swap Team.",
+    "V 1.10 - 3/29/2024",
+    "> Fixed Auto Dodge stop working in the last map(hope so :).",
+    "> Fixed user info webhook show wrong informations.",
+    "> Fixed UI issue that makes some executors does not work.",
+    "V 1.09 - 3/28/2024",
+    "> Added Hide Button For Mobile Users.",
+    "> Improvet the GUI.",
+    "> Improvet Auto Fruit.",
+    "V 1.08 - 3/24/2024",
+    "> Added The New Version of the script.",
+    "> Added Auto Fruit 1 and 2 with 2 methods.",
+    "> Added Auto Raid Challenge.",
+    "> Added New Map to the auto level (I will make it automatic later).",
+    "> I will fix the raid challenge webhook later.",
+    "> script now back to a paid plan and free version to using linkvertise."
+  },
+  Title = "The Intruders",
+  MainText =
+  "Make Sure to join our <b>Discord Server</b>.<br/><font color=\"rgb(0,155,225)\">https://discord.gg/vfkD5VCRKU</font>",
+  DelayToDestroy = 15,
+  From = "ToZxart"
+}
