bebf40e0de3d45343c8794cf460d0f859e83249f
ToZx <69420764+tozxart@users.noreply.github.com>
2025-09-16 12:49:33 +0100

    Create RebirthChampionsUltimateChangeLog.lua

---
+++ ChangeLog/RebirthChampionsUltimateChangeLog.lua
@@ -0,0 +1,16 @@
+return {
+  Version = "0.80",
+  News = {
+    "V 1.14 - 9/16/2025",
+    "> Added Auto Farm Metero .",
+    "> Added Auto Buy Merchant .",
+    "> Added Auto Collect Orbs feature.",
+    "> Added Auto Collect Falling Stars.",
+    "> Complete integration achieved between all automation systems (Auto Mine, Auto Metero, Auto Dungeon, and Auto Tree).",
+    "> Please report any bugs through our Discord server. Additional features in development.",
+  },
+  Title = "The Intruders",
+  MainText = "Make Sure to join our <b>Discord Server</b>.<br/><font color=\"rgb(0,155,225)\">https://discord.gg/vfkD5VCRKU</font><br/><br/>Latest updates include major performance improvements, new gacha systems, and enhanced auto-farming features!",
+  DelayToDestroy = 15,
+  From = "ToZxart"
+}
